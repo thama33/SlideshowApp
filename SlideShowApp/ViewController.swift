dd578283e90dc5f48320ec778806588d7cb0d41f
takuya.hamagami <thama0727@gmail.com>
2022-04-05 08:28:43 +0900

    進む　戻る　ボタンの作成

--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -8,10 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let image1 = UIImage(named: "tree1_haru.png")
+    let image2 = UIImage(named: "ohanami_walk.png")
+    let image3 = UIImage(named: "ohanami_group.png")
+    let image4 = UIImage(named: "yopparai_businessman.png")
+    
+    
+    lazy var gazou = [image1, image2, image3, image4]
+    
+    var junnjo = 0
+    
+    
+    @IBAction func fowardButton(_ sender: Any) {
+        
+        if junnjo == 3 {
+            junnjo = 0
+        }else{
+        junnjo += 1
+        }
+        
+        imageView.image = gazou[junnjo]
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        
+        if junnjo == 0 {
+        junnjo = 3
+        }else{
+            junnjo -= 1
+        }
+    
+    imageView.image = gazou[junnjo]    }
+    
+    @IBAction func playButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
+        
+        imageView.image = image1
     }
 
 
